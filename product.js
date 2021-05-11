4180500a4ca225203a3b62a4f0a3102c33a6d6f7
julien dardennes <jdardennes13170@gmail.com>
2021-05-11 21:43:18 +0200

    mise en place de fonction

--- product.js
+++ product.js
@@ -1,29 +1,28 @@
 let searchParams = new URL(document.location).searchParams;
 let teddyId = searchParams.get("_id");
-console.log(teddyId);
 
 function getTeddy() {
-  fetch("http://localhost:3000/api/teddies/${teddyId}").then((response) => {
-    response.json().then((teddies) => {
-      teddies.for((teddy) => {
-        afficherTeddy(teddy);
-      });
+  fetch(`http://localhost:3000/api/teddies/${teddyId}`).then((response) => {
+    response.json().then((teddy) => {
+      displayTeddy(teddy);
     });
+    console.log(teddyId);
   });
 }
-function afficherTeddy(teddy) {
-  document.getElementById("oursons-product").innerHTML = `
+
+function displayTeddy(teddy) {
+  document.getElementById("teddy-product").innerHTML = `
   <img alt="teddy ${teddy.name}" src="${teddy.imageUrl}">
-                <div class="description">
-                    <h3>${teddy.name}</h3>
-                    <p>${teddy.description}</p>
-                    <h4>Choisissez la couleur:</h4>
-                    <ul>
-                        <li>${teddy.colors}</li>
-                    </ul>
-                </div>
-                <h5>${teddy.price / 100}.00 euros</h5>
-                <a href="panier.html"><input class="validation-panier" type="button" value="Ajouter au panier"></a>
+    <div class="description">
+        <h3>${teddy.name}</h3>
+        <p>${teddy.description}</p>
+        <h4>Choisissez la couleur:</h4>
+        <ul>
+            <li>${teddy.colors}</li>
+        </ul>
+    </div>
+    <h5>${teddy.price / 100}.00 euros</h5>
+    <a href="panier.html"><input class="validation-cart" type="button" value="Ajouter au panier"></a>
   `;
 }
 getTeddy();
--- script.js
+++ script.js
@@ -2,21 +2,21 @@ function getEachTeddy() {
   fetch("http://localhost:3000/api/teddies").then((response) => {
     response.json().then((teddies) => {
       teddies.forEach((teddy) => {
-        afficherTeddy(teddy);
+        displayTeddy(teddy);
       });
     });
   });
 }
 
 //afficher les oursons
-function afficherTeddy(teddy) {
-  document.getElementById("vignettes-oursons").innerHTML += `
-  <div class="oursons-accueil">
+function displayTeddy(teddy) {
+  document.getElementById("thumbnails-teddies").innerHTML += `
+  <div class="teddy-home">
     <a href="./produit.html?_id=${teddy._id}">
       <img alt="teddy ${teddy.name}" src="${teddy.imageUrl}">
       <ul>
         <li>${teddy.name}</li>
-        <li>${teddy.price / 100}.00 euros</li>
+        <li>${teddy.price / 100} €</li>
       </ul>
     </a>
   </div>
